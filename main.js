40fa6f0154b4d9db1d28bf36fc39fdbe51654a7b
javi <javimusama@gmail.com>
2022-06-14 21:26:34 +0200

    Update main.js

--- main.js
+++ main.js
@@ -17,34 +17,6 @@ function onConnectBLEdevice() {
   });
 }
 
-function onReadBLEcharacteristic(){
-     console.log('Reading LED status...');
-    return (bluetoothDeviceCharacteristic.readValue())
-  .then(_ => {
-    console.log('LED status value:');
-    statusLED=bluetoothDeviceCharacteristic.value.getInt8();
-    console.log(statusLED);
-  })
-  .catch(error => {
-    console.log('onConnectBLEdevice Argh! ' + error);
-    console.log(error);
-  }); 
-}
-
-function onWriteBLEcharacteristic(){
-     console.log('Writting LED status...');
-     statusLED++;
-     aux=new Int8Array(1);
-    aux[0]=statusLED;
-    return (bluetoothDeviceCharacteristic.writeValue(aux))
-  .then(_ => {
-    console.log('> Characteristic User Description changed to: ' + Int8Array);
-  })
-  .catch(error => {
-    console.log('Argh! ' + error);
-  });
-}
-
 function requestDevice() {
   console.log('Requesting any Bluetooth Device with 19b10000-e8f2-537e-4f6c-d104768a1214 service...');
   return navigator.bluetooth.requestDevice({
@@ -57,41 +29,6 @@ function requestDevice() {
   });
 }
 
-/*
-function connectDeviceAndCacheCharacteristics() {
-  if (bluetoothDevice.gatt.connected && bluetoothDeviceCharacteristic) {
-    return Promise.resolve();
-  }
-
-  console.log('Connecting to GATT Server...');
-  return bluetoothDevice.gatt.connect()
-  .then(server => {
-    console.log('Getting LED Service...');
-    return server.getPrimaryService('19b10000-e8f2-537e-4f6c-d104768a1214');
-  })
-  .then(service => {
-    console.log('Getting red LED Characteristic...');
-    return service.getCharacteristic('19b10001-e8f2-537e-4f6c-d104768a1215');
-  })
-    .then(characteristic => {
-    bluetoothDeviceCharacteristicRedLED = characteristic;
-  })
-  .then(service => {
-    console.log('Getting green LED Characteristic...');
-    return service.getCharacteristic('19b10001-e8f2-537e-4f6c-d104768a1216');
-  })
-    .then(characteristic => {
-    bluetoothDeviceCharacteristicGreenLED = characteristic;
-  })
-    .then(service => {
-    console.log('Getting blue LED Characteristic...');
-    return service.getCharacteristic('19b10001-e8f2-537e-4f6c-d104768a1217');
-  })
-  .then(characteristic => {
-    bluetoothDeviceCharacteristicBlueLED = characteristic;
-  });
-}
-*/
 
 function connectDeviceAndCacheCharacteristics() {
   if (bluetoothDevice.gatt.connected && bluetoothDeviceCharacteristic) {
@@ -128,17 +65,6 @@ function connectDeviceAndCacheCharacteristics() {
 /* This function will be called when `readValue` resolves and
  * characteristic value changes since `characteristicvaluechanged` event
  * listener has been added. */
-
-function onResetButtonClick() {
-  if (bluetoothDeviceCharacteristic) {
-    bluetoothDeviceCharacteristic.removeEventListener('characteristicvaluechanged',
-        bluetoothDeviceCharacteristic);
-    bluetoothDeviceCharacteristic = null;
-  }
-  // Note that it doesn't disconnect device.
-  bluetoothDevice = null;
-  console.log('> Bluetooth Device reset');
-}
 
 function onDisconnected() {
   console.log('> Bluetooth Device disconnected');

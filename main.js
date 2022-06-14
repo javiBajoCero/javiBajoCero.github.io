8988992dc4361db23318a68f1d8a7f05f6ad504d
javi <javimusama@gmail.com>
2022-06-14 12:13:45 +0200

    Update main.js

--- main.js
+++ main.js
@@ -9,16 +9,16 @@ function onReadBatteryLevelButtonClick() {
   return (bluetoothDevice ? Promise.resolve() : requestDevice())
   .then(connectDeviceAndCacheCharacteristics)
   .then(_ => {
-    log('Reading Battery Level...');
+    console.log('Reading Battery Level...');
     return batteryLevelCharacteristic.readValue();
   })
   .catch(error => {
-    log('Argh! ' + error);
+    console.log('Argh! ' + error);
   });
 }
 
 function requestDevice() {
-  log('Requesting any Bluetooth Device...');
+  console.log('Requesting any Bluetooth Device...');
   return navigator.bluetooth.requestDevice({
    // filters: [...] <- Prefer filters to save energy & show relevant devices.
       acceptAllDevices: true,
@@ -34,14 +34,14 @@ function connectDeviceAndCacheCharacteristics() {
     return Promise.resolve();
   }
 
-  log('Connecting to GATT Server...');
+  console.log('Connecting to GATT Server...');
   return bluetoothDevice.gatt.connect()
   .then(server => {
-    log('Getting Battery Service...');
+    console.log('Getting Battery Service...');
     return server.getPrimaryService('battery_service');
   })
   .then(service => {
-    log('Getting Battery Level Characteristic...');
+    console.log('Getting Battery Level Characteristic...');
     return service.getCharacteristic('battery_level');
   })
   .then(characteristic => {
@@ -58,7 +58,7 @@ function connectDeviceAndCacheCharacteristics() {
  * listener has been added. */
 function handleBatteryLevelChanged(event) {
   let batteryLevel = event.target.value.getUint8(0);
-  log('> Battery Level is ' + batteryLevel + '%');
+  console.log('> Battery Level is ' + batteryLevel + '%');
 }
 
 function onStartNotificationsButtonClick() {
@@ -75,15 +75,15 @@ function onStartNotificationsButtonClick() {
 }
 
 function onStopNotificationsButtonClick() {
-  log('Stopping Battery Level Notifications...');
+  console.log('Stopping Battery Level Notifications...');
   batteryLevelCharacteristic.stopNotifications()
   .then(_ => {
-    log('> Notifications stopped');
+    console.log('> Notifications stopped');
     document.querySelector('#startNotifications').disabled = false;
     document.querySelector('#stopNotifications').disabled = true;
   })
   .catch(error => {
-    log('Argh! ' + error);
+    console.log('Argh! ' + error);
   });
 }
 
@@ -95,13 +95,13 @@ function onResetButtonClick() {
   }
   // Note that it doesn't disconnect device.
   bluetoothDevice = null;
-  log('> Bluetooth Device reset');
+  console.log('> Bluetooth Device reset');
 }
 
 function onDisconnected() {
-  log('> Bluetooth Device disconnected');
+  console.log('> Bluetooth Device disconnected');
   connectDeviceAndCacheCharacteristics()
   .catch(error => {
-    log('Argh! ' + error);
+   console.log('Argh! ' + error);
   });
 }
